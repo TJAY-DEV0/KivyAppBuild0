b1e906acf82bd031e295bae0731d121b31773a48
Tijani Usman <tijaniusman708@gmail.com>
2026-08-06 00:11:55 +0100

    Refactor main to use Kivy for CounterApp

--- src/counter/__main__.py
+++ src/counter/__main__.py
@@ -1,4 +1,15 @@
-from .app import main
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class CounterApp(App):
+    def build(self):
+        return Label(text="Hello from ksproject!")
+
+
+def main():
+    CounterApp().run()
+
 
 if __name__ == "__main__":
     main()
