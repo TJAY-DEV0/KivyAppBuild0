bb2833690fa56597d327d510a7273517ede5bd8d
Tijani Usman <tijaniusman708@gmail.com>
2026-08-07 08:23:36 +0100

    Update __main__.py

--- src/counter/__main__.py
+++ src/counter/__main__.py
@@ -1,15 +1,2 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-
-class CounterApp(App):
-    def build(self):
-        return Label(text="Hello from ksproject!")
-
-
 def main():
     CounterApp().run()
-
-
-if __name__ == "__main__":
-    main()
